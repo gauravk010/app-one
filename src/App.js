3dc0bc84f5f2b80879bc6224390af398fa473a71
Gaurav Kumar <gauravkumar02418@gmail.com>
2024-01-23 14:19:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Hey there I am Gaurav this side!
         </p>
